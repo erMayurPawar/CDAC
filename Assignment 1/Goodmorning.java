4531f0d410a6f268421f8d55cb09f37954d4fb03
Mayur Pawar <153982096+erMayurPawar@users.noreply.github.com>
2024-08-24 12:30:52 +0530

    Add files via upload

---
+++ Assignment 1/Goodmorning.java
@@ -0,0 +1,10 @@
+public class Goodmorning {
+	public static void main (String args[] ) {
+	int time = 5 ; 
+
+	if (time >= 5 && time < 12) {
+            System.out.println("Good Morning");} 
+	else {
+            System.out.println("It's not morning");}
+	}
+}
