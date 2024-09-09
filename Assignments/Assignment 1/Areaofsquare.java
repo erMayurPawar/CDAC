09a460ce74ba76de24d8bd49802c384ee2863f55
Mayur Pawar <153982096+erMayurPawar@users.noreply.github.com>
2024-09-09 10:32:29 +0530

    Delete Assignments directory

--- Assignments/Assignment 1/Areaofsquare.java
+++
@@ -1,8 +0,0 @@
-public class Areaofsquare {
-    public static void main(String[] args) {
-        int side = 5; 
-        int area = side * side;
-
-        System.out.println("The area of the square is: " + area);
-	}
-}
