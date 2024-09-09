34c8fb06898b75b7b90624bc7ea7f9df6515675e
Mayur Pawar <153982096+erMayurPawar@users.noreply.github.com>
2024-09-09 10:34:09 +0530

    Add files via upload

---
+++ Assignment 1/Oddeven.java
@@ -0,0 +1,11 @@
+public class Oddeven {
+	public static void main(String args[]) {
+	
+	int number = 12 ; 
+	if (number % 2 == 0) {
+		System.out.println("Number is Even");}
+		else {
+			System.out.println("Number is Odd");}
+
+	}
+}
---
+++ Assignment 1/Positive.java
@@ -0,0 +1,14 @@
+public class Positive {
+public static void main (String args[]){
+
+	int number = 10 ;
+	if (number>0){
+		
+		System.out.println("The number is positive");
+
+		}else
+		{
+		System.out.println("The number is not positive");
+		}
+	}
+} 
