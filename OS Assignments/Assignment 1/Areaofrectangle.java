59a8534ed6f8f65782edec8c1d137497ea433fb5
Mayur Pawar <153982096+erMayurPawar@users.noreply.github.com>
2024-09-09 10:41:27 +0530

    Add files via upload

---
+++ OS Assignments/Assignment 1/Areaofrectangle.java
@@ -0,0 +1,11 @@
+public class Areaofrectangle {
+	public static void main (String args[]) {
+	
+	int lenght = 5 ; 
+	int width  = 7 ; 
+
+	int area = lenght * width ; 
+
+	System.out.println("Area of Rectangle is :" + area );
+	}
+}
