043df26a5014fdc4ff40d5576930c77d7bc77c00
Mayur Pawar <153982096+erMayurPawar@users.noreply.github.com>
2024-09-09 10:37:37 +0530

    Add files via upload

---
+++ OS/OS Assignments/Assignment 1/Negative.java
@@ -0,0 +1,14 @@
+public class Negative{
+	public static void main (String args[]){
+	
+	int number = -5 ; 
+
+	if (number < 0){
+		System.out.println("Number is Negative ");}
+	else {
+		System.out.println("Number is Not Negative ");}
+	    
+
+	}
+
+}
